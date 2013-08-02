dee2e8d775083e95f8909419d8b24e19f100701b
Nancy Do <odycnan@gmail.com>
2013-08-02 10:49:18 -0700

    Processes the graph, seems to be working for examples 0, 1, and 2.

--- solution.py
+++ solution.py
@@ -4,7 +4,7 @@ class Vertex:
 	    self.parents = set()
 
 # open the input file
-file = open("input/1", "r")
+file = open("input/2", "r")
 
 graph = {}
 
@@ -22,6 +22,26 @@ for line in file:
 	if output not in graph:
 		graph[output] = Vertex()
 	graph[output].parents.add(input)
+
+# process the graph to get rid of vertices which have
+# one input and one output
+for key, value in graph.iteritems():
+	# connect neighbors directly
+	if len(value.parents) == 1 and len(value.children) == 1:
+		parent = list(value.parents)[0]
+		child = list(value.children)[0]
+
+		# replace the parent's child with this key's child
+		graph[parent].children.add(child)
+		graph[parent].children.remove(key)
+
+		# similarly, replace the child's parent with this key's parent
+		graph[child].parents.add(parent)
+		graph[child].parents.remove(key)
+
+		# Removing the item from the dictionary causes problems with
+		# the loop. Instead, mark it as having no parents and children.
+		value.parents = value.children = 0
 
 # print the graph
 for key, value in graph.iteritems():
