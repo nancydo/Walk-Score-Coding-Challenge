0a8cc5041d5e0a0f2ddb201bf342bbf59b38675a
Nancy Do <odycnan@gmail.com>
2013-08-02 11:11:05 -0700

    Now reads/writes to stdin/stdout.

--- solution.py
+++ solution.py
@@ -1,27 +1,28 @@
+import sys
+
 class Vertex:
 	def __init__(self):
 	    self.children = set()
 	    self.parents = set()
 
-# open the input file
-file = open("input/2", "r")
 
 graph = {}
 
 # create the graph
-for line in file:
+for line in sys.stdin:
 	line = line.rstrip("\n");
 	edge = line.split("\t")
-	input = edge[0]
-	output = edge[1]
+	if len(edge) == 2:
+		input = edge[0]
+		output = edge[1]
 
-	if input not in graph:
-		graph[input] = Vertex();
-	graph[input].children.add(output)
+		if input not in graph:
+			graph[input] = Vertex();
+		graph[input].children.add(output)
 
-	if output not in graph:
-		graph[output] = Vertex()
-	graph[output].parents.add(input)
+		if output not in graph:
+			graph[output] = Vertex()
+		graph[output].parents.add(input)
 
 # process the graph to get rid of vertices which have
 # one input and one output
